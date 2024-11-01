14676c5472caac834fb4a93203267355bfe13c1c
Hoang-Tien-Manh-Duc <khongcotinhban@gmail.com>
2024-11-01 16:23:28 +0700

    add(object): add object demo_list_employees_full_stack

---
+++ ems-frontend/src/app.jsx
@@ -0,0 +1,28 @@
+import './app.css'
+import EmployeeComponent from './components/EmployeeComponent';
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+export function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/* //http:/localhost:3000 */}
+            <Route path='/' element={<ListEmployeeComponent />}></Route>
+            {/* //http:/localhost:3000/employees */}
+            <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+            {/* //http:/localhost:3000/add-employee */}
+            <Route path='/add-employee' element={<EmployeeComponent />}></Route>
+            {/* //http:/localhost:3000/update-employee */}
+            <Route path='/update-employee/:id' element={<EmployeeComponent />}></Route>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
+    </>
+  )
+}
